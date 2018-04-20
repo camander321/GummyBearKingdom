420655dd993e1239efd4a295c7d6eeb6f29dbdd2
camander321 <chamburg321@gmail.com>
2018-04-20 10:45:48 -0700

    add product details page

--- GummyBearKingdom/GummyBearKingdom/Controllers/ProductsController.cs
+++ GummyBearKingdom/GummyBearKingdom/Controllers/ProductsController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using GummyBearKingdom.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,9 +15,16 @@ namespace GummyBearKingdom.Controllers
     public class ProductsController : Controller
     {
         private StoreDbContext db = new StoreDbContext();
+
         public IActionResult Index()
         {
             List<Product> model = db.Products.ToList();
+            return View(model);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product model = db.Products.FirstOrDefault(product => product.ProductId == productId);
             return View(model);
         }
     }
