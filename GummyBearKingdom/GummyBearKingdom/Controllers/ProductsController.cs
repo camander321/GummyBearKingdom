b9b534b36dfddc01e4f6a2b311e8adb1759ded09
camander321 <chamburg321@gmail.com>
2018-04-20 10:22:29 -0700

    add product table

--- GummyBearKingdom/GummyBearKingdom/Controllers/ProductsController.cs
+++ GummyBearKingdom/GummyBearKingdom/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using GummyBearKingdom.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,9 +11,11 @@ namespace GummyBearKingdom.Controllers
 {
     public class ProductsController : Controller
     {
+        private StoreDbContext db = new StoreDbContext();
         public IActionResult Index()
         {
-            return View();
+            List<Product> model = db.Products.ToList();
+            return View(model);
         }
     }
 }
