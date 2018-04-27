67ee48c5072080e36aed719f06fdb2d3d142b02f
camander321 <chamburg321@gmail.com>
2018-04-27 12:52:14 -0700

    add review repository class

---
+++ GummyBearKingdom/GummyBearKingdom/Models/Repositories/EFReviewRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GummyBearKingdom.Models
+{
+    public class EFReviewRepository : IReviewRepository
+    {
+        GummyBearDbContext db;
+        public EFReviewRepository()
+        {
+            db = new GummyBearDbContext();
+        }
+        public EFReviewRepository(GummyBearDbContext thisDb)
+        {
+            db = thisDb;
+        }
+
+        public IQueryable<Review> Reviews { get { return db.Reviews; } }
+
+        public Review Edit(Review review)
+        {
+            db.Entry(review).State = EntityState.Modified;
+            db.SaveChanges();
+            return review;
+        }
+
+        public void Remove(Review review)
+        {
+            db.Remove(review);
+            db.SaveChanges();
+        }
+
+        public void RemoveAll()
+        {
+            db.Reviews.RemoveRange(db.Reviews);
+            db.SaveChanges();
+        }
+
+        public Review Save(Review review)
+        {
+            db.Reviews.Add(review);
+            db.SaveChanges();
+            return review;
+        }
+    }
+}
---
+++ GummyBearKingdom/GummyBearKingdom/Models/Repositories/IReviewRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GummyBearKingdom.Models
+{
+    public interface IReviewRepository
+    {
+        IQueryable<Review> Reviews { get; }
+        Review Save(Review review);
+        Review Edit(Review review);
+        void Remove(Review review);
+        void RemoveAll();
+    }
+}
