9ec778c940479bbca72bbfc08857543d8db5f91f
camander321 <chamburg321@gmail.com>
2018-04-27 11:23:03 -0700

    add product controller tests for getting correct actionresult

--- GummyBearKingdom/GummyBearKingdom.Tests/ControllerTests/ProductsControllerTests.cs
+++ GummyBearKingdom/GummyBearKingdom.Tests/ControllerTests/ProductsControllerTests.cs
@@ -27,6 +27,7 @@ namespace GummyBearKingdom.Controllers.Tests
         public void Dispose()
         {
             mock.Object.RemoveAll();
+            db.RemoveAll();
         }
 
         [TestMethod]
@@ -35,20 +36,86 @@ namespace GummyBearKingdom.Controllers.Tests
             DbSetup();
             ProductsController controller = new ProductsController(mock.Object);
             var result = controller.Index();
-            Assert.IsInstanceOfType(result, typeof(ActionResult));
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
         }
-
         [TestMethod]
-        public void Mock_IndexContainsModelData_List() // Confirms model as list of items
+        public void Mock_GetViewResultDetails_ActionResult() // Confirms route returns view
         {
             DbSetup();
-            ViewResult indexView = new ProductsController(mock.Object).Index() as ViewResult;
-            var result = indexView.ViewData.Model;
-            Assert.IsInstanceOfType(result, typeof(List<Product>));
+            ProductsController controller = new ProductsController(mock.Object);
+            var result = controller.Details(2);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+        [TestMethod]
+        public void Mock_GetViewResultCreateGet_ActionResult() // Confirms route returns view
+        {
+            DbSetup();
+            ProductsController controller = new ProductsController(mock.Object);
+            var result = controller.Create();
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+        [TestMethod]
+        public void Mock_GetViewResultCreatePost_ActionResult() // Confirms route returns view
+        {
+            DbSetup();
+            Product product = new Product { ProductId = 3, Name = "Test 3", Description = "Its one more test", Cost = 6 };
+            ProductsController controller = new ProductsController(mock.Object);
+            var result = controller.Create(product);
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+        }
+        [TestMethod]
+        public void Mock_GetViewResultEditGet_ActionResult() // Confirms route returns view
+        {
+            DbSetup();
+            ProductsController controller = new ProductsController(mock.Object);
+            var result = controller.Edit(2);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+        [TestMethod]
+        public void Mock_GetViewResultEditPost_ActionResult() // Confirms route returns view
+        {
+            DbSetup();
+            Product product = new Product { ProductId = 3, Name = "Test 3", Description = "Its one more test", Cost = 6 };
+            ProductsController controller = new ProductsController(mock.Object);
+            var result = controller.Edit(product);
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+        }
+        [TestMethod]
+        public void Mock_GetViewResultDeleteGet_ActionResult() // Confirms route returns view
+        {
+            DbSetup();
+            ProductsController controller = new ProductsController(mock.Object);
+            var result = controller.Delete(2);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+        [TestMethod]
+        public void Mock_GetViewResultDeletePost_ActionResult() // Confirms route returns view
+        {
+            DbSetup();
+            Product product = new Product { ProductId = 3, Name = "Test 3", Description = "Its one more test", Cost = 6 };
+            ProductsController controller = new ProductsController(mock.Object);
+            var result = controller.DeleteConfirmed(2);
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+        }
+        [TestMethod]
+        public void Mock_GetViewResultDeleteAllGet_ActionResult() // Confirms route returns view
+        {
+            DbSetup();
+            ProductsController controller = new ProductsController(mock.Object);
+            var result = controller.DeleteAll();
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+        [TestMethod]
+        public void Mock_GetViewResultDeleteAllPost_ActionResult() // Confirms route returns view
+        {
+            DbSetup();
+            ProductsController controller = new ProductsController(mock.Object);
+            var result = controller.DeleteAllConfirmed();
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
         }
 
         [TestMethod]
-        public void Mock_IndexModelContainsAnimals_Collection()
+        public void Mock_IndexModelContainsProducts_Collection()
         {
             DbSetup();
             ProductsController controller = new ProductsController(mock.Object);
@@ -61,16 +128,29 @@ namespace GummyBearKingdom.Controllers.Tests
         }
 
         [TestMethod]
-        public void Mock_PostViewResultCreate_ViewResult()
+        public void Mock_DetailsModelContainsProduct_Product()
         {
-            Product product = new Product { ProductId = 3, Name = "Test 2", Description = "Its another test", Cost = 6 };
-
             DbSetup();
+            Product product = mock.Object.Products.FirstOrDefault();
             ProductsController controller = new ProductsController(mock.Object);
-            var resultView = controller.Create(product);
-            Assert.IsInstanceOfType(resultView, typeof(RedirectToActionResult));
-
+            var resultView = controller.Details(product.ProductId) as ViewResult;
+            var model = resultView.ViewData.Model as Product;
+            Assert.IsInstanceOfType(model, typeof(Product));
+            Assert.AreEqual(product, model);
         }
+
+        [TestMethod]
+        public void Mock_EditGetModelContainsProduct_Product()
+        {
+            DbSetup();
+            Product product = mock.Object.Products.FirstOrDefault();
+            ProductsController controller = new ProductsController(mock.Object);
+            var resultView = controller.Edit(product.ProductId) as ViewResult;
+            var model = resultView.ViewData.Model as Product;
+            Assert.IsInstanceOfType(model, typeof(Product));
+            Assert.AreEqual(product, model);
+        }
+
 
         [TestMethod]
         public void DB_CreatesNewEntries_Collection()
