9d063a2291631849088ba69beb0f8783e08822a5
camander321 <chamburg321@gmail.com>
2018-04-27 12:16:11 -0700

    finish tests for product model

--- GummyBearKingdom/GummyBearKingdom.Tests/ModelTests/ProductTests.cs
+++ GummyBearKingdom/GummyBearKingdom.Tests/ModelTests/ProductTests.cs
@@ -16,7 +16,7 @@ namespace GummyBearKingdom.Models.Tests
         EFProductRepository db = new EFProductRepository(new TestDbContext());
         public void Dispose()
         {
-            //db.RemoveAll();
+            db.RemoveAll();
         }
 
         [TestMethod]
@@ -30,12 +30,48 @@ namespace GummyBearKingdom.Models.Tests
         public void DB_SaveAddsNewProduct_Product()
         {
             Product product = new Product { Name = "Test 2", Description = "Its another test", Cost = 6 };
-            List<Product> testList = new List<Product>();
-
-            testList.Add(product);
+            List<Product> testList = new List<Product> { product };
             db.Save(product);
 
             CollectionAssert.AreEqual(testList, db.Products.ToList());
+        }
+
+        [TestMethod]
+        public void DB_EditModifiesDbItem_Product()
+        {
+            string startName = "this is the product's old name";
+            string newName = "this is the product's new name";
+            Product product = new Product { Name = startName, Description = "Its another test", Cost = 6 };
+            db.Save(product);
+
+            product.Name = newName;
+            db.Edit(product);
+
+            Assert.AreEqual(newName, db.Products.FirstOrDefault(p => p.ProductId == product.ProductId).Name);
+        }
+
+        [TestMethod]
+        public void DB_RemoveDeletesProduct_Void()
+        {
+            Product product = new Product { Name = "Test 2", Description = "Its another test", Cost = 6 };
+            db.Save(product);
+            CollectionAssert.Contains(db.Products.ToList(), product);
+
+            db.Remove(product);
+
+            CollectionAssert.DoesNotContain(db.Products.ToList(), product);
+        }
+
+        [TestMethod]
+        public void DB_RemoveAllClearsTable_Void()
+        {
+            db.Save(new Product { Name = "Test 1", Description = "Its another test", Cost = 6 });
+            db.Save(new Product { Name = "Test 2", Description = "Its one more test", Cost = 7 });
+            Assert.AreEqual(2, db.Products.ToList().Count());
+
+            db.RemoveAll();
+
+            Assert.AreEqual(0, db.Products.ToList().Count());
         }
     }
 }
